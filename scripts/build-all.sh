e1e7afe94a45803c308df4ab364d2fca66cfd9a1
Damien Lespiau <damien.lespiau@intel.com>
2016-03-02 14:29:38 +0000

    scripts: Use '...' to recurse in all packages
    Signed-off-by: Damien Lespiau <damien.lespiau@intel.com>

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -3,8 +3,4 @@
 script_dir=$(cd `dirname $0`; pwd)
 root_dir=`dirname $script_dir`
 
-for e in `ls $root_dir/examples`; do
-	echo "Building $e"
-	cd $root_dir/examples/$e
-	go build
-done
+(cd $root_dir && go build -v ./...)
