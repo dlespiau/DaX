25bf7e663e9b07522ea07d59cf255fc7991c742a
Damien Lespiau <damien.lespiau@intel.com>
2016-03-02 14:37:37 +0000

    scripts: Use go test ./... to run all sub-packages tests
    Signed-off-by: Damien Lespiau <damien.lespiau@intel.com>

--- scripts/test-all.sh
+++ scripts/test-all.sh
@@ -4,5 +4,4 @@ script_dir=$(cd `dirname $0`; pwd)
 root_dir=`dirname $script_dir`
 
 $script_dir/build-all.sh
-echo "Testing dax/math"
-(cd $root_dir && go test math)
+(cd $root_dir && go test ./...)
