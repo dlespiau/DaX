101bf2c9bef94aba60f6ef679258423167a742a9
Damien Lespiau <damien.lespiau@gmail.com>
2016-12-21 20:22:18 +0000

    tests: Remove debugging messages
    Signed-off-by: Damien Lespiau <damien.lespiau@gmail.com>

--- lib/color_test.go
+++ lib/color_test.go
@@ -11,8 +11,6 @@ func testEqualF(t *testing.T, expected, value float32, epsilon float64) {
 	if math.Abs(expected) < 1e-3 {
 		assert.True(t, math.Abs(expected-value) < float32(epsilon))
 	} else {
-		t.Logf("Expected %f, value %f\n", expected, value)
-		t.Logf("ratio %f\n", (expected-value)/expected)
 		assert.InEpsilon(t, expected, value, epsilon)
 	}
 }
@@ -41,22 +39,20 @@ func TestHSL(t *testing.T) {
 		{"#000080", 0, 0, 128, 240. / 360, 1, 1. / 4},
 	}
 
-	for i, test := range tests {
+	for _, test := range tests {
 		var color Color
 
 		color.FromRGBu8(test.r, test.g, test.b)
 		h, s, l := color.ToHSL()
-		t.Logf("Test case #%d", i)
 		assert.Equal(t, test.h, h)
 		assert.Equal(t, test.s, s)
 		testEqualF(t, test.l, l, 1e-2)
 	}
 
-	for i, test := range tests {
+	for _, test := range tests {
 		var color Color
 
 		color.FromHSL(test.h, test.s, test.l)
-		t.Logf("Test case #%d", i)
 		testEqualF(t, color.R, float32(test.r)/255, 1e-2)
 		testEqualF(t, color.G, float32(test.g)/255, 1e-2)
 		testEqualF(t, color.B, float32(test.b)/255, 1e-2)
