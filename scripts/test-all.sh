549c7dbebfca9f027ebf553385478dc17e866267
Damien Lespiau <damien.lespiau@gmail.com>
2016-12-22 00:00:18 +0000

    tests: Add race detector and timeout to tests
    The sooner we add the race detector, the better. Also limit tests to 5s:
    if we end up in a deadlock, the test will fail after that timeout.
    
    Signed-off-by: Damien Lespiau <damien.lespiau@gmail.com>

--- scripts/test-all.sh
+++ scripts/test-all.sh
@@ -3,12 +3,14 @@
 script_dir=$(cd `dirname $0`; pwd)
 root_dir=`dirname $script_dir`
 
+TEST_FLAGS="-v -race -timeout 5s"
+
 function test_travis
 {
 	echo "mode: count" > profile.cov
 
 	for pkg in $(cat $root_dir/.test-packages.txt); do
-		go test -v -covermode=count -coverprofile=profile_tmp.cov $pkg || exit 1
+		go test $TEST_FLAGS -covermode=count -coverprofile=profile_tmp.cov $pkg
 		[ -f profile_tmp.cov ] && tail -n +2 profile_tmp.cov >> profile.cov;
 		rm -f profile_tmp.cov
 	done
@@ -16,7 +18,7 @@ function test_travis
 
 function test_local
 {
-	go test -v `cat $root_dir/.test-packages.txt`
+	go test $TEST_FLAGS `cat $root_dir/.test-packages.txt`
 }
 
 if [ "$CI" = "true" ]; then
